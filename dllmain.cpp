d486609e5617e253b27f68b8c29d53283dbdfb9e
Nishimura-Katsuo <jwebbb01@hotmail.com>
2020-06-14 13:01:24 -0600

    Updated the console window creation

--- dllmain.cpp
+++ dllmain.cpp
@@ -10,16 +10,54 @@
 #include <cstdio>
 #include <cwchar>
 #include <iostream>
+#include <io.h>
+#include <fcntl.h>
 
+using std::cout;
 using std::wcout;
-using std::endl;
+using std::cin;
+using std::wcin;
+using std::cerr;
+using std::wcerr;
 
 void OpenConsole() {
+    //Create a console for this application
     AllocConsole();
-    FILE* fDummy;
-    freopen_s(&fDummy, "CONIN$", "r", stdin);
-    freopen_s(&fDummy, "CONOUT$", "w", stderr);
-    freopen_s(&fDummy, "CONOUT$", "w", stdout);
+
+    // Get STDOUT handle
+    HANDLE ConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+    int SystemOutput = _open_osfhandle(intptr_t(ConsoleOutput), _O_TEXT);
+    FILE* COutputHandle = _fdopen(SystemOutput, "w");
+
+    // Get STDERR handle
+    HANDLE ConsoleError = GetStdHandle(STD_ERROR_HANDLE);
+    int SystemError = _open_osfhandle(intptr_t(ConsoleError), _O_TEXT);
+    FILE* CErrorHandle = _fdopen(SystemError, "w");
+
+    // Get STDIN handle
+    HANDLE ConsoleInput = GetStdHandle(STD_INPUT_HANDLE);
+    int SystemInput = _open_osfhandle(intptr_t(ConsoleInput), _O_TEXT);
+    FILE* CInputHandle = _fdopen(SystemInput, "r");
+
+    //make cout, wcout, cin, wcin, wcerr, cerr, wclog and clog point to console as well
+    std::ios::sync_with_stdio(true);
+
+    // Redirect the CRT standard input, output, and error handles to the console
+    freopen_s(&CInputHandle, "CONIN$", "r", stdin);
+    freopen_s(&COutputHandle, "CONOUT$", "w", stdout);
+    freopen_s(&CErrorHandle, "CONOUT$", "w", stderr);
+
+    //Clear the error state for each of the C++ standard stream objects. We need to do this, as
+    //attempts to access the standard streams before they refer to a valid target will cause the
+    //iostream objects to enter an error state. In versions of Visual Studio after 2005, this seems
+    //to always occur during startup regardless of whether anything has been read from or written to
+    //the console or not.
+    std::wcout.clear();
+    std::cout.clear();
+    std::wcerr.clear();
+    std::cerr.clear();
+    std::wcin.clear();
+    std::cin.clear();
 }
 
 void LoadDLL(LPWSTR moduleName) {
@@ -36,6 +74,7 @@ void CheckParams() {
     for (int c = 0; c < argc; c++) {
         if (wcscmp(argv[c], consoleFlag) == 0) {
             OpenConsole();
+            break;
         }
     }
 
